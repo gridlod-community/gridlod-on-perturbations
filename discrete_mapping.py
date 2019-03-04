0f3a9e5f1a337c1105fefc4edfb963a9004ee374
Tim Keil <tim.keil@wwu.de>
2019-03-04 13:38:25 +0100

    [discrete_mapping] remove hack

--- discrete_mapping.py
+++ discrete_mapping.py
@@ -47,18 +47,7 @@ class MappingCQ1:
         return func.evaluateCQ1(self.N, self.inv_mapping, x)
 
     def J(self, x):
-        derivative = func.evaluateCQ1D(self.N, self.mapping, x)
-        #This is a very stupid hack
-        for n in range(np.shape(derivative)[0]):
-            for i in range(2):
-                for j in range(2):
-                    if derivative[n][i][j] < 1e-10:
-                        if i == j:
-                            derivative[n][i][j] = 0.5
-                        else:
-                            derivative[n][i][j] = 0
-
-        return derivative
+        return func.evaluateCQ1D(self.N, self.mapping, x)
 
     def Jinv(self, x):
         return np.linalg.inv(self.J(x))
